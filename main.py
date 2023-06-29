ce7e13155214400f00349322bf3e4e376dee25da
Gowri-Mohan <118054081+Gowri-Mohan@users.noreply.github.com>
2023-06-29 12:43:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-a = int(input("enter first number = "))
-b = int(input("enter second number = "))
-s = a + b
-print("the sum is = ", s)
+from PIL import Image
+
+filepath = str(input("enter filepath = "))
+image = Image.open(filepath)
+x = int(input("enter pixel width = "))
+y = int(input("enter pixel height = "))
+new_image = image.resize((x, y), resample=0)
+new_image.show()
